f88b92a11a47b097773a5e2757b41d55272367b1
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-07-26 19:49:32 -0500

    feeling like I am getting the hang of loops

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -18,14 +18,20 @@ console.log(supplyChanges);
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
 
-
+console.log(supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
 
+supplyChanges.pop();
+console.log(supplyChanges);
+
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+
+supplyChanges.push(25);
+console.log(supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -34,6 +40,18 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
 
+for (let changes of supplyChanges) {
+    if (changes > 0); {
+        console.log("Added ", changes, " parts.");
+    }
+    if (changes === 0); {
+        console.log("No Change.");
+    }
+    if (changes < 0); {
+        console.log("Removed ", changes, " Parts");
+    }
+
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
